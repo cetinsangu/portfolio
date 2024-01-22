2606347e7532bd54e89991b6dab77b380f5dcaee
cetinsangu <sangucetin@gmail.com>
2024-01-22 11:12:53 +0300

    font changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "300",
+});
 
 const Layout = ({ children, className = "" }) => {
   return (
     <div
-      className={`w-full h-full inline-block z-0 bg-light dark:bg-dark p-32 xl:p-24 lg:p-16 md:p-12 sm:p-8 ${className}`}
+      className={`w-full h-full inline-block z-0 bg-light dark:bg-dark p-32 xl:p-24 lg:p-16 md:p-12 sm:p-8 ${className} ${roboto.className}`}
     >
       {children}
     </div>
